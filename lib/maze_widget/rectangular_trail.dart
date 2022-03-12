a797836ee0968e8bc44e9bc1bc082f23c2230439
Jonathan <jonathan016@binus.ac.id>
2022-03-12 21:24:20 +0700

    Refactored trail generation

--- lib/maze_widget/rectangular_trail.dart
+++ lib/maze_widget/rectangular_trail.dart
@@ -15,21 +15,92 @@ class RectangularTrail extends StatelessWidget {
 
   late final SizedBox _defaultEmptyTrail =
       SizedBox(width: tileSize, height: tileSize);
-  static const _borderSide = BorderSide(color: Colors.red, width: 2);
 
   double get _getHalfTileSize => tileSize / 2;
+
+  static const _verticalDivider = VerticalDivider(
+    thickness: 2,
+    color: Colors.red,
+  );
+
+  static const _horizontalDivider = Divider(
+    thickness: 2,
+    color: Colors.red,
+  );
+
+  late final Widget _halfVerticalTop = SizedBox(
+    width: tileSize,
+    height: _getHalfTileSize,
+    child: _verticalDivider,
+  );
+
+  late final Widget _halfVerticalBottom = SizedBox(
+    width: tileSize,
+    height: tileSize,
+    child: Column(
+      verticalDirection: VerticalDirection.up,
+      children: [
+        SizedBox(
+          width: tileSize,
+          height: _getHalfTileSize,
+          child: _verticalDivider,
+        )
+      ],
+    ),
+  );
+
+  late final Widget _halfHorizontalLeft = SizedBox(
+    width: _getHalfTileSize,
+    height: tileSize,
+    child: _horizontalDivider,
+  );
+
+  late final Widget _halfHorizontalRight = SizedBox(
+    width: tileSize,
+    height: tileSize,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          width: _getHalfTileSize,
+          height: tileSize,
+          child: _horizontalDivider,
+        )
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     switch (inDirection) {
       case Direction.up:
-        return _getTrailForInDirectionUp();
+        return Stack(
+          children: [
+            _halfVerticalBottom,
+            _getTrailForInDirectionUp(),
+          ],
+        );
       case Direction.right:
-        return _getTrailForInDirectionRight();
+        return Stack(
+          children: [
+            _halfHorizontalLeft,
+            _getTrailForInDirectionRight(),
+          ],
+        );
       case Direction.down:
-        return _getTrailForInDirectionDown();
+        return Stack(
+          children: [
+            _halfVerticalTop,
+            _getTrailForInDirectionDown(),
+          ],
+        );
       case Direction.left:
-        return _getTrailForInDirectionLeft();
+        return Stack(
+          children: [
+            _halfHorizontalRight,
+            _getTrailForInDirectionLeft(),
+          ],
+        );
       default:
         return _defaultEmptyTrail;
     }
@@ -38,54 +109,11 @@ class RectangularTrail extends StatelessWidget {
   Widget _getTrailForInDirectionUp() {
     switch (outDirection) {
       case Direction.right:
-        return Column(children: [
-          SizedBox(width: tileSize, height: _getHalfTileSize),
-          Row(
-            children: [
-              SizedBox(width: _getHalfTileSize, height: _getHalfTileSize),
-              Container(
-                width: _getHalfTileSize,
-                height: _getHalfTileSize,
-                decoration: const BoxDecoration(
-                    border: Border(left: _borderSide, top: _borderSide)),
-              )
-            ],
-          )
-        ]);
+        return _halfHorizontalRight;
       case Direction.left:
-        return Column(
-          children: [
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(bottom: _borderSide)),
-            ),
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(right: _borderSide)),
-            ),
-          ],
-        );
+        return _halfHorizontalLeft;
       case Direction.up:
-        return Column(
-          children: [
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(right: _borderSide)),
-            ),
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(right: _borderSide)),
-            ),
-          ],
-        );
+        return _halfVerticalTop;
       default:
         return _defaultEmptyTrail;
     }
@@ -94,43 +122,11 @@ class RectangularTrail extends StatelessWidget {
   Widget _getTrailForInDirectionRight() {
     switch (outDirection) {
       case Direction.down:
-        return Column(
-          children: [
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration: const BoxDecoration(
-                border: Border(bottom: _borderSide),
-              ),
-            ),
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration: const BoxDecoration(
-                border: Border(right: _borderSide),
-              ),
-            ),
-          ],
-        );
+        return _halfVerticalBottom;
       case Direction.right:
-        return Row(
-          children: [
-            Container(
-              width: tileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(bottom: _borderSide)),
-            )
-          ],
-        );
+        return _halfHorizontalRight;
       case Direction.up:
-        return Container(
-          width: _getHalfTileSize,
-          height: _getHalfTileSize,
-          decoration: const BoxDecoration(
-            border: Border(bottom: _borderSide, right: _borderSide),
-          ),
-        );
+        return _halfVerticalTop;
       default:
         return _defaultEmptyTrail;
     }
@@ -139,68 +135,11 @@ class RectangularTrail extends StatelessWidget {
   Widget _getTrailForInDirectionLeft() {
     switch (outDirection) {
       case Direction.down:
-        return Column(
-          children: [
-            Row(
-              children: [
-                SizedBox(width: _getHalfTileSize, height: _getHalfTileSize),
-                Container(
-                  width: _getHalfTileSize,
-                  height: _getHalfTileSize,
-                  decoration: const BoxDecoration(
-                    border: Border(bottom: _borderSide),
-                  ),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                  width: _getHalfTileSize,
-                  height: _getHalfTileSize,
-                  decoration: const BoxDecoration(
-                    border: Border(right: _borderSide),
-                  ),
-                ),
-                SizedBox(width: _getHalfTileSize, height: _getHalfTileSize)
-              ],
-            ),
-          ],
-        );
+        return _halfVerticalBottom;
       case Direction.left:
-        return Row(
-          children: [
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(bottom: _borderSide)),
-            ),
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(bottom: _borderSide)),
-            ),
-          ],
-        );
+        return _halfHorizontalLeft;
       case Direction.up:
-        return Row(
-          children: [
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(right: _borderSide)),
-            ),
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(bottom: _borderSide)),
-            ),
-          ],
-        );
+        return _halfVerticalTop;
       default:
         return _defaultEmptyTrail;
     }
@@ -209,47 +148,11 @@ class RectangularTrail extends StatelessWidget {
   Widget _getTrailForInDirectionDown() {
     switch (outDirection) {
       case Direction.down:
-        return Column(
-          children: [
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(right: _borderSide)),
-            ),
-            Container(
-              width: _getHalfTileSize,
-              height: _getHalfTileSize,
-              decoration:
-                  const BoxDecoration(border: Border(right: _borderSide)),
-            ),
-          ],
-        );
+        return _halfVerticalBottom;
       case Direction.left:
-        return Container(
-          width: _getHalfTileSize,
-          height: _getHalfTileSize,
-          decoration: const BoxDecoration(
-            border: Border(
-              right: _borderSide,
-              bottom: _borderSide,
-            ),
-          ),
-        );
+        return _halfHorizontalLeft;
       case Direction.right:
-        return Row(children: [
-          Container(
-            width: _getHalfTileSize,
-            height: _getHalfTileSize,
-            decoration: const BoxDecoration(border: Border(right: _borderSide)),
-          ),
-          Container(
-            width: _getHalfTileSize,
-            height: _getHalfTileSize,
-            decoration:
-                const BoxDecoration(border: Border(bottom: _borderSide)),
-          ),
-        ]);
+        return _halfHorizontalRight;
       default:
         return _defaultEmptyTrail;
     }
